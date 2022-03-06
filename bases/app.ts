455e8e4a6f7f3b4d8a13ab2f6c8b3d977bef4b0e
Jhon Moofarry <jhon.moofarry00@usc.edu.co>
2022-03-06 11:09:07 -0500

    ex

--- bases/app.ts
+++ bases/app.ts
@@ -1,9 +1,44 @@
+(() => {
 
-(() => { 
-  const a : number = 10;
+  // Tipos
+  const batman:string = 'Bruce';
+  const superman:string = 'Clark';
 
-  console.log(a);
+  const existe:boolean = false;
 
- })
+  // Tuples
+  const parejaHeroes:[string,string] = [batman,superman];
+  const villano:[string,number,boolean] = ['Lex Luto',5,true];
 
+  // Arreglos
+  const allies:string[] = ['Mujer Maravilla','Aqua-man','San', 'Flash'];
+
+  //Enumeraciones
+  enum Power {
+    fuerzaAquaman,
+    fuerzaBatman,
+    fuerzaFlash=5,
+    fuerzaSuperman=100,
+  }
+  const fuerzaFlash:Power =  Power.fuerzaAquaman
+  const fuerzaSuperman:Power =  Power.fuerzaBatman
+  const fuerzaBatman:Power = Power.fuerzaFlash
+  const fuerzaAcuaman:Power =  Power.fuerzaSuperman
+
+  // Retorno de funciones
+  function activar_batiseñal():string{
+    return 'activada';
+  }
+
+  function pedir_ayuda():void{
+    console.log('Auxilio!!!');
+  }
+
+  // Aserciones de Tipo
+  const poder: any = '100';
+  const largoDelPoder:number = (poder as string).length;
+  console.log( largoDelPoder );
+
+
+})()
 
